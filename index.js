acc02b2bf84baeee1b5ba5d8ee075327afadd7b6
Naman Goel <goelnaman66@gmail.com>
2022-02-05 11:20:27 +0530

    Step 2: Initial homepage routing

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const router = require('./src/routes/index');
+
+
+app.use('/',router);        // anything that comes to this url will be handled by this router
 
 
 app.listen(3000,(req,res)=>{
